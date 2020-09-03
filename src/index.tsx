b45e5b38a9cf94c7ddce0783645c6c0cdfe77d49
quavious <leenowon97@gmail.com>
2020-09-03 19:40:05 +0900

    initial commit
    typescript intro and why parameters are called props and type interface

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+type AppProps = {
+  title: string;
+  color: string;
+}
+
+function App(props:AppProps){
+  const {title, color} = props;
+  return (
+    <>
+      <h1>{title}</h1>
+      <h2>{color}</h2>
+    </>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title="Tech Hello?" color="red"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +26,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
